7375b742c276d312bff19ff30e126276933550cf
sunhuwh <764503410@qq.com>
2016-09-18 11:24:14 +0800

    fix simple factory bugs

--- src/mian/java/com/sh/simple/Factory.java
+++ src/mian/java/com/sh/simple/Factory.java
@@ -2,7 +2,7 @@ package com.sh.simple;
 
 public class Factory {
     
-    public Parent createProduct(String column){
+    public static Parent createProduct(String column){
         Parent p = null;
         if("one".equals(column)){
             p = new ChildrenOne();
--- src/mian/java/com/sh/simple/Test.java
+++ src/mian/java/com/sh/simple/Test.java
@@ -3,8 +3,8 @@ package com.sh.simple;
 public class Test {
     
     public static void main(String[] args) {
-        Factory f = new Factory();
-        Parent p = f.createProduct("two");
+        Parent p;
+        p = Factory.createProduct("two");
         System.out.println(p.car());
     }
     
